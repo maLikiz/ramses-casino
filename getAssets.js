9a3f7b33b69500ef3a2b7a907562dea7fb4cbcec
Nikita Malykh <malixzu@gmail.com>
2019-05-21 21:55:08 +0500

    Create getAssets.js

---
+++ getAssets.js
@@ -0,0 +1,28 @@
+const fetch = require('node-fetch');
+const fs = require('fs');
+const website = 'https://ramses.casino';
+
+const files = [
+  {
+    nameToSave: './src/css/style.css',
+    toGet: '/css/style.css',
+  },
+  {
+    nameToSave: './src/js/script.js',
+    toGet: '/js/script.js',
+  },
+];
+
+files.forEach(async file => {
+  const url = `${website}${file.toGet}`;
+  const response = await fetch(url);
+  const result = await response.text();
+
+  fs.writeFileSync(file.nameToSave, result, error => {
+    if (error) throw console.error(error);
+  });
+});
+
+const gettingFiles = files.map(item => `\`${item.nameToSave}\``).join(',\n');
+
+console.log(`Files:\n${gettingFiles}\n - has been updated!`)
